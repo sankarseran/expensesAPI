c987a3f79699d110ff695965adae42d3643cce25
Sankaralingam Seranthian <sankaralingam.psg@gmail.com>
2023-07-14 00:38:47 +0200

    updates

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.post('/login', authController.login);
 app.get("/people", authenticateToken, async (request, response) => { return response.send({'res': [1, 2, 3, 4, 5]})});// Expenses controller
 app.use('/expenses', authenticateToken, expensesRouter);
 
-const port = 3000;
-app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+const PORT = process.env.PORT || 3030;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
 });
